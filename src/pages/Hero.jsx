00143028da978f1a39d0339287fcf73b99f8dc52
AlienX <aditya1115.ar@gmail.com>
2024-03-27 12:33:45 +0530

    added bg in hero

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="h-full p-6 md:px-10 lg:px-5 max-w-[1024px] mx-auto">
-      <p className="text-[24px] lg:text-[64px] font-semibold bg-gradient-to-b from-[#FFFFFF] to-gray-700 bg-clip-text text-transparent leading-tight">
-        Connecting clients to <br />
-        the global <br />
-        commodities market
+    <div className="relative p-6 md:px-10 lg:px-5 max-w-[1024px] mx-auto h-[100vh] overflow-hidden flex flex-col items-center justify-between">
+      <video
+        autoPlay
+        loop
+        muted
+        className="md:absolute top-0 left-0 w-full h-auto z-0"
+      >
+        <source src="./bg.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <p className="text-[24px] md:text-[44px] font-semibold bg-gradient-to-b from-[#FFFFFF] to-gray-700 bg-clip-text text-transparent leading-tight z-10 relative">
+        Connecting clients to the global commodities market
       </p>
-      <button className="mt-8 text-center bg-[#0066FF] h-[40px] max-w-[110px]  py-2 px-4 rounded-[8px] text-[#FFFFFF] font-semibold text-[14px]">
+      <button className="mt-8 mb-14 text-center bg-[#0066FF] h-[40px] max-w-[110px]  py-2 px-4 rounded-[8px] text-[#FFFFFF] font-semibold text-[14px] z-10 relative">
         Contact Us
       </button>
     </div>
