cc993a672954737d66f7187ee5017b8824cbbb2e
Aditya <89471821+dev15Aditya@users.noreply.github.com>
2024-04-15 19:29:56 +0530

    Update Image.jsx

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -14,10 +14,12 @@ export default function Image({ image }) {
         alt="people"
         className="w-[240px] h-[350px] cursor-pointer  rounded-lg"
         onMouseEnter={handleClick}
+        onClick={handleClick}
       />
       <div
         className="w-[240px] h-[350px] cursor-pointer flex flex-col justify-center rounded-lg bg-white/20 shadow-md backdrop-blur-5 border border-white/30"
         onMouseLeave={handleClick}
+        onClick={handleClick}
       >
         <h1 className="text-white text-xl font-semibold text-center">
           {image[1]}
